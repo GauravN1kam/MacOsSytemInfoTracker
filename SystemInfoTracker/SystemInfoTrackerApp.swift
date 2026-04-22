eab5d718acf66b87782650f1eccf6a214efd95b3
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-04-22 23:34:03 +0530

    Initial commit

---
+++ SystemInfoTracker/SystemInfoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemInfoTrackerApp.swift
+//  SystemInfoTracker
+//
+//  Created by Gaurav Nikam on 19/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SystemInfoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
